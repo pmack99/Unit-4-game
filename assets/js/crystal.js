9739a5abc4f47f88394ced890c11e591d826260b
PMack2 <Prestonmack@gmail.com>
2018-08-31 10:40:12 -0400

    initial

---
+++ assets/js/crystal.js
@@ -0,0 +1,11 @@
+var magicNumber;
+var yourNumber;
+
+
+
+$(".card-img-top").on("click", function() {
+
+    // Clicking the button triggers an alert message.
+    alert("You clicked a crystal!");
+
+  });
